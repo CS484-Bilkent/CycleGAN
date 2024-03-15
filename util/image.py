4de8eb703f2b0e2883a7fcf2ccf0f2a6c6c103bb
Berke Gökmen <ahmetberkegokmen@icloud.com>
2024-03-15 15:10:34 +0300

    add autocast?

--- util/image.py
+++ util/image.py
@@ -8,10 +8,10 @@ import torchvision.utils as vutils
 
 def save_combined_image(fake_a, real_a, fake_b, real_b, epoch, i, run_name):
     # Select the first image in the batch for each tensor
-    fake_a = fake_a[0]
-    real_a = real_a[0]
-    fake_b = fake_b[0]
-    real_b = real_b[0]
+    fake_a = fake_a[0] * 0.5 + 0.5
+    real_a = real_a[0] * 0.5 + 0.5
+    fake_b = fake_b[0] * 0.5 + 0.5
+    real_b = real_b[0] * 0.5 + 0.5
 
     fig, axs = plt.subplots(2, 2, figsize=(10, 10))
     axs = axs.ravel()
@@ -21,7 +21,7 @@ def save_combined_image(fake_a, real_a, fake_b, real_b, epoch, i, run_name):
 
     for ax, img, title in zip(axs, images, titles):
         img = img.detach().cpu().numpy()  # Move to cpu
-        img = (img - img.min()) / (img.max() - img.min())  # Normalize to [0, 1]
+        # img = (img - img.min()) / (img.max() - img.min())  # Normalize to [0, 1]
         img = np.transpose(img, (1, 2, 0))  # Change from CxHxW to HxWxC for matplotlib
 
         ax.imshow(img)
