951c91e1b11bbaca7de4aea1c2d72b20cffa51ca
Berke Gökmen <ahmetberkegokmen@icloud.com>
2024-03-30 16:07:28 +0300

    dataset script fix

--- dataset.sh
+++ dataset.sh
@@ -2,8 +2,8 @@ dataset_name="monet2photo"
 
 cd ../datasets && \
     curl -O "https://efrosgans.eecs.berkeley.edu/cyclegan/datasets/${dataset_name}.zip" && \
-    unzip {dataset_name}.zip && \
-    rm {dataset_name}.zip && \
+    unzip ${dataset_name}.zip && \
+    rm ${dataset_name}.zip && \
     mkdir train && \
     mkdir test && \
     mv ./trainA ./train/trainA && \
