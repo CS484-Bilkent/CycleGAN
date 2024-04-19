45d4dd462a53c0d012ad6f872e5cc48260b17e69
gencturkmert <gencturkmert1@gmail.com>
2024-04-19 11:00:46 +0300

    inference code, consistent image saving

--- scripts/infer.py
+++ scripts/infer.py
@@ -12,7 +12,7 @@ from dataset.dataset import ABDataset
 from dataset.dataset import img_transform as transform
 from config.args import parser
 from torch.utils.data import DataLoader
-from util.image import save_combined_image
+from util.image import *
 from tqdm import tqdm
 
 
@@ -60,11 +60,18 @@ def main(args):
     for i, (real_a, real_b) in tqdm_loop:
         real_a = real_a.to(args.device)
         real_b = real_b.to(args.device)
-        fake_b = generator_A(real_a)
-        fake_a = generator_A(real_b)
-        save_combined_image(fake_b, real_a, fake_a, real_b, 0, i, f"test/{args.run_name}")
 
-        if args.test_limit and i > args.test_limit:
+        # generate fake ones
+        fake_b = generator_A(real_a)  # A to B
+        fake_a = generator_B(real_b)  # B to A
+
+        # regenerate for cycle consistency
+        rec_a = generator_B(fake_b)  # Fake B to A
+        rec_b = generator_A(fake_a)  # Fake A to B
+
+        save_cycle_consistent_images(real_a, fake_b, rec_a, real_b, fake_a, rec_b, 0, i, f"test/{args.run_name}")
+
+        if args.test_limit and i >= args.test_limit:
             break
 
 
